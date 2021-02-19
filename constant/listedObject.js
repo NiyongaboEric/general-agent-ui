e6103c506502027355af36cca2b9404e73f92abe
NiyongaboEric <niyongaboeric69@gmail.com>
2021-02-19 21:58:32 +0200

    ft(landing-page): user should visit the landing page
    	- add logo image
            - add partner images
    	- add custom navbar
    	- add custom footer
    	- make page responsiveness
    
    	- [Finishes #176996602]

---
+++ constant/listedObject.js
@@ -0,0 +1,42 @@
+import {
+  KINYARWANDA_LANGUAGE_MESSAGE,
+  ENGLISH_LANGUAGE_MESSAGE
+} from './message';
+
+export const languageListProperties = [
+  {
+    language: 'Kinyarwanda',
+    imgPath: '/assets/images/rwanda_flag.svg',
+    imgAlt: KINYARWANDA_LANGUAGE_MESSAGE,
+  },
+  {
+    language: 'English',
+    imgPath: '/assets/images/usa_flag.svg',
+    imgAlt: ENGLISH_LANGUAGE_MESSAGE,
+  }
+];
+
+export const listPartners = [
+  {
+    imgName: 'Entrepreneurial Solutions Partners',
+    imgAlt: 'Running Tourism Inc Program',
+    imgPath: '/assets/images/esp.png',
+  },
+  {
+    imgName: 'Rwanda Development Board',
+    imgAlt: 'Sponsoring sponsor',
+    imgPath: '/assets/images/rdb.png',
+  },
+  {
+    imgName: 'Mastercard',
+    imgAlt: 'Funding program',
+    imgPath: '/assets/images/mastercard.png',
+  }
+];
+
+export const advertisingImage = [
+  {
+    imgPath: '/assets/images/dummy-image.jpg',
+    imgAlt: 'Everyone is ordering to hit the expect number',
+  }
+];
